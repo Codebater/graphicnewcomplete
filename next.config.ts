a07990eb23cf14b7e8af735c119ab05de1c44aff
Codebater <117357627+AsadullahMaliyawala@users.noreply.github.com>
2026-04-02 19:37:50 +0500

    fixes

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  experimental: {
-    serverComponentsExternalPackages: ['mongoose'],
-  },
+  serverExternalPackages: ['mongoose'],
   images: {
     domains: ['localhost'],
     unoptimized: true,
